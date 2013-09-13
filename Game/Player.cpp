c02685e6e1ec9292e5086d5fdef2de02e3ae1ca2
Simon Ducharme Boutin <sducharme@omnialabs.net>
2013-09-13 12:57:54 -0400

    Adding the Player class

---
+++ Game/Player.cpp
@@ -0,0 +1,12 @@
+/*****************************************************************************************
+ * Desc: Utility methods for working paths setup
+ *****************************************************************************************/
+
+#include "Defines.h"
+#include "Player.h"
+
+bool Player::setName(char* sName) {
+
+	return false;
+}
+
---
+++ Game/Player.h
@@ -0,0 +1,14 @@
+/*****************************************************************************************
+ * Desc: Utility methods for working paths setup
+ *****************************************************************************************/
+
+#ifndef _Player_h
+#define _Player_h
+
+    
+class Player {
+	public:
+	    bool setName(char* sName);
+};
+
+#endif
